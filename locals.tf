33f83e8d9d2b97ac643b1b2c0b7ccb5add59f7f3
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-03-28 15:18:55 +0200

    Time window option

--- locals.tf
+++ locals.tf
@@ -8,32 +8,39 @@ locals {
         "alr-prd-heartbeat-ux-law-perf-crit-01": {
             description = "Alert when Heartbeat Stopped"
             query_path = "${local.path}/unix_heartbeat.kusto"
+            time_window = 2280
         }
         "alr-prd-diskspace-ux-law-perf-warn-crit-01": {
             description = "Alert when Filespace runs out of space"
             query_path = "${local.path}/unix_filespace.kusto"
+            time_window = 2280
         }
         "alr-prd-diskspace-ux-law-perf-warn-crit-02": {
             description = "Alert when SAP Filespace runs out of space"
             query_path = "${local.path}/unix_sap_filespace.kusto"
+            time_window = 2280
         }
 
         "alr-prd-eventlog6008-win-law-event-crit-01": {
             description = "Alert when the Windows event log entries occurred"
             query_path = "${local.path}/windows_event_6008.kusto"
+            time_window = 30
         }
         "alr-prd-eventlog55-win-law-event-crit-01": {
             description = "Alert when the Windows event log entries occurred"
             query_path = "${local.path}/windows_event_55.kusto"
+            time_window = 30
         }
 
         "alr-prd-heartbeat-win-law-perf-crit-01": {
             description = "Alert when Heartbeat Stopped"
             query_path = "${local.path}/windows_heartbeat.kusto"
+            time_window = 2280
         }
         "alr-prd-diskspace-win-law-perf-warn-crit-01": {
             description = "Alert when Filespace runs out of space"
             query_path = "${local.path}/windows_filespace.kusto"
+            time_window = 2280
         }
     }
 }
