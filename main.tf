fe396b160a8de3d5717b160c40d5d5e233278a48
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-03-29 16:50:03 +0200

    descriptions and input object

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_log_analytics_datasource_windows_event" "application" {
   name                = "lad-application"
-  resource_group_name = local.law_rg
-  workspace_name      = local.law_name
+  resource_group_name = var.log_analytics_workspace.resource_group_name
+  workspace_name      = var.log_analytics_workspace.name
   event_log_name      = "Application"
   event_types         = ["Error", "Warning", "Information"]
 }
 
 resource "azurerm_log_analytics_datasource_windows_event" "system" {
   name                = "lad-system"
-  resource_group_name = local.law_rg
-  workspace_name      = local.law_name
+  resource_group_name = var.log_analytics_workspace.resource_group_name
+  workspace_name      = var.log_analytics_workspace.name
   event_log_name      = "System"
   event_types         = ["Error", "Warning", "Information"]
 }
@@ -18,10 +18,10 @@ module "vm_insights" {
   source  = "qbeyond/log-analytics-VMInsights/azurerm"
   version = "1.0.2"
   log_analytics_workspace = {
-    id                  = var.law_id
-    name                = local.law_name
-    resource_group_name = local.law_rg
-    location            = var.location
+    id                  = var.log_analytics_workspace.id
+    name                = var.log_analytics_workspace.name
+    resource_group_name = var.log_analytics_workspace.resource_group_name
+    location            = var.log_analytics_workspace.location
   }
 
   depends_on = [
@@ -32,7 +32,7 @@ module "vm_insights" {
 
 resource "azurerm_monitor_action_group" "action_group" {
   name                = "EventPipelineCentral_AG_1"
-  resource_group_name = local.law_rg
+  resource_group_name = var.log_analytics_workspace.resource_group_name
   short_name          = "monitorhook"
 
   webhook_receiver {
@@ -45,12 +45,12 @@ resource "azurerm_monitor_action_group" "action_group" {
 resource "azurerm_monitor_scheduled_query_rules_alert" "rules" {
   for_each = local.rules
   name                = each.key
-  location            = var.location
-  resource_group_name = local.law_rg
+  location            = var.log_analytics_workspace.location
+  resource_group_name = var.log_analytics_workspace.resource_group_name
   action {
     action_group = [azurerm_monitor_action_group.action_group.id]
   }
-  data_source_id = var.law_id
+  data_source_id = var.log_analytics_workspace.id
   description    = each.value.description
   enabled        = true
   # Count all requests with server error result code grouped into 5-minute bins
