9560e8a7dd021f0104418b8956b3bddd6e458e1f
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-03-30 14:50:23 +0200

    Added variable for additional custom queries

--- locals.tf
+++ locals.tf
@@ -1,15 +1,15 @@
 locals {
     path = "${path.module}/queries"
 
-    rules = {
+    rules = merge({
         "alr-prd-heartbeat-ux-law-perf-crit-01": {
             description = "Alert when Heartbeat of unix machines Stopped"
-            query_path = "${local.path}/unix_heartbeat.kusto"
+            query_path  = "${local.path}/unix_heartbeat.kusto"
             time_window = 2280
         }
         "alr-prd-diskspace-ux-law-perf-warn-crit-01": {
             description = "Alert when filesystem of unix runs out of space"
-            query_path = "${local.path}/unix_filespace.kusto"
+            query_path  = "${local.path}/unix_filespace.kusto"
             time_window = 2280
         }
         "alr-prd-diskspace-ux-law-perf-warn-crit-02": {
@@ -20,24 +20,24 @@ locals {
 
         "alr-prd-eventlog6008-win-law-event-crit-01": {
             description = "Alert when the Windows event 6008 (unexpected shutdown) was logged"
-            query_path = "${local.path}/windows_event_6008.kusto"
+            query_path  = "${local.path}/windows_event_6008.kusto"
             time_window = 30
         }
         "alr-prd-eventlog55-win-law-event-crit-01": {
             description = "Alert when the Windows event 55 (disk corruption) was logged"
-            query_path = "${local.path}/windows_event_55.kusto"
+            query_path  = "${local.path}/windows_event_55.kusto"
             time_window = 30
         }
 
         "alr-prd-heartbeat-win-law-perf-crit-01": {
             description = "Alert when Heartbeat from Windows machines Stopped"
-            query_path = "${local.path}/windows_heartbeat.kusto"
+            query_path  = "${local.path}/windows_heartbeat.kusto"
             time_window = 2280
         }
         "alr-prd-diskspace-win-law-perf-warn-crit-01": {
             description = "Alert when filesystem of windows runs out of space"
-            query_path = "${local.path}/windows_filespace.kusto"
+            query_path  = "${local.path}/windows_filespace.kusto"
             time_window = 2280
         }
-    }
+    }, var.additional_queries)
 }
--- variables.tf
+++ variables.tf
@@ -9,12 +9,21 @@ variable "log_analytics_workspace" {
 }
 
 variable "webhook_name" {
-    type = string
-    description = "Name of the alert webhook"
+  type = string
+  description = "Name of the alert webhook"
 }
 
 variable "webhook_service_uri" {
-    type = string
-    description = "Link to the webhook receiver URL"
+  type = string
+  description = "Link to the webhook receiver URL"
 }
 
+variable "additional_queries" {
+  type = map({
+    query_path  = string
+    description = string
+    time_window = number
+  })
+  description = "List of additional alert rule queries to create with a file path, description and time_window"
+  default = []
+}
