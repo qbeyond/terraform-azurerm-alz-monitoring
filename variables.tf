4d88ebbe95a07c0b40a318edb14a05e8eae74535
Chiara Holzgräfe <chiara.holzgraefe@qbeyond.de>
2023-07-20 15:58:49 +0200

    edit variables to simplify usage add example configuration to extra_queries

--- variables.tf
+++ variables.tf
@@ -5,22 +5,21 @@ variable "log_analytics_workspace" {
     resource_group_name = string
     location            = string
     workspace_id        = string
-    primary_shared_key  = string
   })
-  description = "Log Analytics Worksapce that all VMs are connected to for monitoring"
-  nullable = false
+  description = "Log Analytics Worksapce that all VMs are connected to for monitoring."
+  nullable    = false
 }
 
 variable "webhook_name" {
-  type = string
-  description = "Name of the alert webhook"
-  nullable = false
+  type        = string
+  description = "Name of the alert webhook."
+  nullable    = false
 }
 
 variable "webhook_service_uri" {
-  type = string
-  description = "Link to the webhook receiver URL"
-  nullable = false
+  type        = string
+  description = "Link to the webhook receiver URL."
+  nullable    = false
 }
 
 variable "additional_queries" {
@@ -29,36 +28,30 @@ variable "additional_queries" {
     description = string
     time_window = number
   }))
-  description = "List of additional alert rule queries to create with a file path, description and time_window"
-  default = {}
-  nullable = false
+  description = "List of additional alert rule queries to create with a file path, description and time_window."
+  default     = {}
+  nullable    = false
 }
 
 variable "event_pipeline_key" {
-    type = string
-    description = "Function key provided by monitoring team"
-    sensitive = true
-}
-
-variable "resource_group" {
-  type = object({
-    name = string 
-    location = string
-    id = string
-  })
-  description = "Resource Group"
-  nullable = false
+  type        = string
+  description = "Function key provided by monitoring team."
+  sensitive   = true
 }
 
 variable "automation_account" {
   type = object({
-    name = string
-    id = string
+    name                = string
+    id                  = string
+    location            = string
+    resource_group_name = string
   })
-  description = "automation account for the resource graph"
-  nullable = false
+  description = "Automation account where the resource graph script will be deployed."
+  nullable    = false
 }
 
-variable "subscription_id" {
-    type = string
+variable "primary_shared_key" {
+  type        = string
+  description = "Primary shared key of the central monitoring LAW."
+  sensitive   = true
 }
