312fb28eec9d324b5e2ff28ec7fe9e1f3f11e780
Chiara Holzgräfe <chiara.holzgraefe@qbeyond.de>
2023-07-20 14:20:54 +0200

    add resourcegraph configuration

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,6 +2,9 @@ provider "azurerm" {
   features{}
 }
 
+data "azurerm_subscription" "current" {
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "rg-Monitor-dev-01"
   location = "westeurope"
@@ -15,14 +18,20 @@ resource "azurerm_log_analytics_workspace" "example" {
   retention_in_days   = 30
 }
 
+resource "azurerm_automation_account" "example" {
+  name = "aac-Management-Monitor-dev-01"
+  sku_name = "Basic"
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
 module "monitor" {
   source                  = "../.."
-  log_analytics_workspace = {
-    id                  = azurerm_log_analytics_workspace.example.id
-    name                = azurerm_log_analytics_workspace.example.name
-    resource_group_name = azurerm_log_analytics_workspace.example.resource_group_name
-    location            = azurerm_log_analytics_workspace.example.location
-  }
+  log_analytics_workspace = azurerm_log_analytics_workspace.example
   webhook_name            = "QBY EventPipeline"
   webhook_service_uri     = "https://function-app.azurewebsites.net/api/Webhook"
-}
+  resource_group          = azurerm_resource_group.example
+  automation_account      = azurerm_automation_account.example
+  subscription            = data.azurerm_subscription.current
+  event_pipeline_key      = "key"
+  }
---
+++ resourcegraph.tf
@@ -0,0 +1,84 @@
+resource "azurerm_automation_module" "az_accounts" {
+  name                    = "Az.Accounts"
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+
+  module_link {
+    uri = "https://devopsgallerystorage.blob.core.windows.net:443/packages/az.accounts.2.12.1.nupkg"
+  } # version variable?
+}
+
+resource "azurerm_automation_module" "az_resourcegraph" {
+  name                    = "Az.Resourcegraph"
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  module_link {
+    uri = "https://devopsgallerystorage.blob.core.windows.net:443/packages/az.resourcegraph.0.13.0.nupkg"
+  }
+  depends_on = [azurerm_automation_module.az_accounts]
+}
+
+resource "azurerm_automation_runbook" "resourcegraph_query" {
+  name                    = "Import-ResourceGraphToLogAnalytics"
+  location                = var.resource_group.location
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "This runbook imports results from defined azure resource graph query to log analytics workspace."
+  runbook_type            = "PowerShell"
+  content                 = file("${path.module}/resourcegraph/Import-ResourceGraphToLogAnalytics.ps1")
+}
+
+resource "azurerm_automation_variable_string" "law_sharedkey" {
+  name                    = "law_sharedkey"
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  value                   = var.log_analytics_workspace.primary_shared_key
+  encrypted               = true
+}
+
+resource "azurerm_automation_variable_string" "default_subscription" {
+  name                    = "default_subscription"
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  value                   = var.subscription.id
+}
+
+resource "time_static" "automation_schedule_tomorrow_5am" {
+  rfc3339 = timeadd(formatdate("YYYY-MM-DD'T'05:00:00Z",timestamp()), "24h")
+  triggers = {
+    aac_name  = var.automation_account.name
+    aac_id    = var.automation_account.id
+    rg_name   = var.resource_group.name
+    rg_id     = var.resource_group.id
+    frequency = "Day"
+    interval  = 1
+    timezone  = "Europe/Berlin"
+  }
+}
+
+resource "azurerm_automation_schedule" "daily" {
+  name                    = "aas-Import-ResourceGraphToLogAnalytics-Once-Daily"
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  frequency               = time_static.automation_schedule_tomorrow_5am.triggers.frequency
+  interval                = time_static.automation_schedule_tomorrow_5am.triggers.interval
+  timezone                = time_static.automation_schedule_tomorrow_5am.triggers.timezone
+  start_time              = time_static.automation_schedule_tomorrow_5am.rfc3339
+}
+
+resource "azurerm_automation_job_schedule" "resourcegraph_query" {
+  resource_group_name     = var.resource_group.name
+  automation_account_name = var.automation_account.name
+  schedule_name           = azurerm_automation_schedule.daily.name
+  runbook_name            = azurerm_automation_runbook.resourcegraph_query.name
+
+  parameters = {
+   query                    = file("${path.module}/resourcegraph/resource.kusto")
+   managementgroupidtocheck = "alz"
+   logtype                  = "MonitoringResources"
+   customerid               = var.log_analytics_workspace.workspace_id
+  }
+}
+
