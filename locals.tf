c56070bf9221fb582463815a14551ca1bedac6af
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-12-04 16:26:58 +0100

    rename alert rules according to naming convention

--- locals.tf
+++ locals.tf
@@ -2,42 +2,47 @@ locals {
     path = "${path.module}/queries"
 
     rules = merge({
-        "alr-prd-heartbeat-ux-law-perf-crit-01": {
+        "alr-prd-Heartbeat-ux-law-log-crit-01": {
             description = "Alert when Heartbeat of unix machines Stopped"
             query_path  = "${local.path}/unix_heartbeat.kusto"
             time_window = 2280
         }
-        "alr-prd-diskspace-ux-law-perf-warn-crit-01": {
+        "alr-prd-Diskspace-ux-law-log-warn-crit-01": {
             description = "Alert when filesystem of unix runs out of space"
             query_path  = "${local.path}/unix_filespace.kusto"
             time_window = 2280
         }
-        "alr-prd-diskspace-ux-law-perf-warn-crit-02": {
+        "alr-prd-Diskspace-ux-law-log-warn-crit-02": {
             description = "Alert when SAP Filespace runs out of space"
             query_path = "${local.path}/unix_sap_filespace.kusto"
             time_window = 2280
         }
 
-        "alr-prd-eventlog6008-win-law-event-crit-01": {
+        "alr-prd-Eventlog6008-win-law-log-crit-01": {
             description = "Alert when the Windows event 6008 (unexpected shutdown) was logged"
             query_path  = "${local.path}/windows_event_6008.kusto"
             time_window = 30
         }
-        "alr-prd-eventlog55-win-law-event-crit-01": {
+        "alr-prd-Eventlog55-win-law-log-crit-01": {
             description = "Alert when the Windows event 55 (disk corruption) was logged"
             query_path  = "${local.path}/windows_event_55.kusto"
             time_window = 30
         }
 
-        "alr-prd-heartbeat-win-law-perf-crit-01": {
+        "alr-prd-Heartbeat-win-law-log-crit-01": {
             description = "Alert when Heartbeat from Windows machines Stopped"
             query_path  = "${local.path}/windows_heartbeat.kusto"
             time_window = 2280
         }
-        "alr-prd-diskspace-win-law-perf-warn-crit-01": {
+        "alr-prd-Diskspace-win-law-log-warn-crit-01": {
             description = "Alert when filesystem of windows runs out of space"
             query_path  = "${local.path}/windows_filespace.kusto"
             time_window = 2280
+        }
+        "alr-prd-VMBackup-bkp-law-log-warn-01": {
+            description = "Alert when a VM backup job fails"
+            query_path  = "${local.path}/vm_backup.kusto"
+            time_window = 
         }
     }, var.additional_queries)
 }
