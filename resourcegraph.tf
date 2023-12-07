27c083916027505d29fd961e4cc9be9491f485ae
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-12-07 10:09:41 +0100

    Added issue comment

--- resourcegraph.tf
+++ resourcegraph.tf
@@ -61,6 +61,7 @@ resource "azurerm_automation_schedule" "daily" {
   start_time              = time_static.automation_schedule_tomorrow_5am.rfc3339
 }
 
+# Will allways be recreated to fix: https://github.com/hashicorp/terraform-provider-azurerm/issues/17970
 resource "azurerm_automation_job_schedule" "resourcegraph_query" {
   resource_group_name     = var.automation_account.resource_group_name
   automation_account_name = var.automation_account.name
