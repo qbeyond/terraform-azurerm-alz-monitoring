eaf40ecc66595b6953c38bd2b10394b8b7dcde20
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2024-02-29 15:31:15 +0100

    moved DCRs

---
+++ data_collection_rules.tf
@@ -0,0 +1,77 @@
+resource "azurerm_monitor_data_collection_rule" "vm_insight" {
+  name                        = "dcr-prd-all-VmInsights-01"
+  resource_group_name         = var.log_analytics_workspace.resource_group_name
+  location                    = var.log_analytics_workspace.location
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = var.log_analytics_workspace.id
+      name                  = var.log_analytics_workspace.name
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-ServiceMap"]
+    destinations = [var.log_analytics_workspace.name]
+  }
+
+  data_flow { 
+    streams      = ["Microsoft-InsightsMetrics"]
+    destinations = [var.log_analytics_workspace.name]
+  }
+
+  data_sources {
+    extension {
+      extension_name     = "DependencyAgent"
+      input_data_sources = []
+      name               = "DependencyAgent"
+      streams            = ["Microsoft-ServiceMap"]
+    }
+
+    performance_counter {
+        counter_specifiers            = ["\\VmInsights\\DetailedMetrics"]
+        name                          = "VMInsightsPerfCounters"
+        sampling_frequency_in_seconds = 60
+        streams                       = ["Microsoft-InsightsMetrics"]
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  description = "Central DCR to collect performance counter metrics and dependency agent data for azure monitor"
+}
+
+resource "azurerm_monitor_data_collection_rule" "event_log" {
+  name                        = "dcr-prd-win-EventLogBasic-01"
+  resource_group_name         = var.log_analytics_workspace.resource_group_name
+  location                    = var.log_analytics_workspace.location
+  kind                        = "Windows"
+
+  destinations {
+    log_analytics {
+      workspace_resource_id = var.log_analytics_workspace.id
+      name                  = var.log_analytics_workspace.name
+    }
+  }
+
+  data_flow {
+    streams      = ["Microsoft-Event"]
+    destinations = [var.log_analytics_workspace.name]
+  }
+  
+  data_sources {
+    windows_event_log {
+      name     = "EventLog"
+      streams  = ["Microsoft-Event"]
+      x_path_queries = ["Application!*[System[EventID=55]]", "Application!*[System[EventID=6008]]"]
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  description = "Collects EventIDs 55 and 6008 from the EventLog"
+}
