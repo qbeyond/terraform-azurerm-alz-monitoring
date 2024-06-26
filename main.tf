c6ed0c2549a95e397e1ae1287b1b2e2b56475200
MMAR <markus.maring@qbeyond.de>
2024-06-26 16:10:40 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "azurerm_subscription" "current" {
 }
 
 resource "azurerm_monitor_action_group" "eventpipeline" {
-  count = var.event_pipeline_config.enabled ? 1 : 0
+  count               = var.event_pipeline_config.enabled ? 1 : 0
   name                = "EventPipelineCentral_AG_1"
   resource_group_name = var.log_analytics_workspace.resource_group_name
   short_name          = "monitorhook"
@@ -16,33 +16,33 @@ resource "azurerm_monitor_action_group" "eventpipeline" {
 }
 
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "this" {
-  for_each = local.rules
-  name = each.key
-  location = var.log_analytics_workspace.location
+  for_each            = local.rules
+  name                = each.key
+  location            = var.log_analytics_workspace.location
   resource_group_name = var.log_analytics_workspace.resource_group_name
-  tags = var.tags
+  tags                = var.tags
 
   action {
     action_groups = local.action_group
   }
 
-  scopes = [var.log_analytics_workspace.id]
+  scopes      = [var.log_analytics_workspace.id]
   description = each.value.description
-  enabled = true
-  severity = 0
+  enabled     = true
+  severity    = 0
 
   evaluation_frequency = "PT5M"
-  window_duration = each.value.time_window
+  window_duration      = each.value.time_window
   criteria {
-    query = file(each.value.query_path)
+    query                   = file(each.value.query_path)
     time_aggregation_method = "Count"
-    operator = "GreaterThan"
-    threshold = 0
+    operator                = "GreaterThan"
+    threshold               = 0
 
     dimension {
-      name = "RawData"
+      name     = "RawData"
       operator = "Include"
-      values = ["*"]
+      values   = ["*"]
     }
 
     resource_id_column = "_ResourceId"
