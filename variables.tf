f32bf976dae9f3a615097469fd07ed93ada74d85
Chiara Holzgräfe <chiara.holzgraefe@qbeyond.de>
2024-07-31 16:37:42 +0200

    add custom json log add dce

--- variables.tf
+++ variables.tf
@@ -11,6 +11,15 @@ variable "log_analytics_workspace" {
   nullable    = false
 }
 
+variable "resource_group" {
+  type = object({
+    id                  = string
+    name                = string
+    location            = string
+  })
+  
+}
+
 variable "additional_queries" {
   type = map(object({
     query_path  = string
