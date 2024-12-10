ca63d5334c01e8f953c348e48dd75593a22d70f3
Chiara Holzgräfe <chiara.holzgraefe@qbeyond.de>
2024-12-10 15:10:29 +0100

    add event selection

--- event_ids.tf
+++ event_ids.tf
@@ -1,40 +1,37 @@
 locals {
+  selected_events = merge({}, var.active_services.active_directory == true ? local.active_directory : {}, var.active_services.managed_os == true ? local.managed_os : {}, var.active_services.mssql == true ? local.mssql : {})
+
   active_directory = {
     "1008" = {
-      area = "AD"
-      severity = "critical"
+      area      = "AD"
+      severity  = "critical"
       winsource = "Microsoft-Windows-ActiveDirectory_DomainService"
     }
     "1126" = {
-      area = "AD"
-      severity = "critical"
+      area      = "AD"
+      severity  = "critical"
       winsource = "Microsoft-Windows-ActiveDirectory_DomainService"
     }
     "1567" = {
-      area = "AD"
-      severity = "critical"
+      area      = "AD"
+      severity  = "critical"
       winsource = "Microsoft-Windows-ActiveDirectory_DomainService"
     }
   }
+
   managed_os = {
     "17137" = {
-      area = "NT"
-      severity = "Information"
+      area      = "NT"
+      severity  = "Information"
       winsource = "MSSQL$EXPR_CM_530"
-    } 
+    }
   }
 
-  temp_test = templatefile("queries/windows_event.kusto.tftpl", {
-            "all_events" = {
-        "1008" = {
-          "area"      = "AD"
-          "severity"  = "critical"
-          "winsource" = "Microsoft-Windows-ActiveDirectory_DomainService"
-        }
-        "17137" = {
-          "area"      = "NT"
-          "severity"  = "Information"
-          "winsource" = "MSSQL$EXPR_CM_530"
-      }}
-    })
+  mssql = {
+    "17061" = {
+      area      = "MSSQL"
+      severity  = "Warning"
+      winsource = " "
+    }
+  }
 }
