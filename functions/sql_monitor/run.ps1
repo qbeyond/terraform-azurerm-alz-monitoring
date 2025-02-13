a500ad9522d419a7684ea0994e0de4a34d50fccb
Bärisch <maurice.baerisch@qbeyond.de>
2025-02-13 15:28:35 +0100

    revert: feat!(mssql_monitoring): remove tenant-wide dbscan
    This reverts commit 970e5b4f31169b730dbd0121cb2a57fe24527fb2.

--- functions/sql_monitor/run.ps1
+++ functions/sql_monitor/run.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Monitors MSSQL SLA and sends availability to our Qby Event Pipeline.
+    Retrieving Monitoring Data and sending it to our Event Pipeline.
 .DESCRIPTION
     This script regularly checks SQL Servers for availability and sends the retrieved data to our Event Pipeline.
 
@@ -26,6 +26,7 @@
     - Event Pipeline not reachable -> ?
     - Wrong SQL credentials -> return CRITICAL with error info
     - No permissions (managed identity not working) -> ?
+.EXAMPLE
 #>
 
 param(
@@ -33,6 +34,23 @@ param(
 )
 
 #region helper_functions
+function Get-QbyDatabasesInTenant {
+    param ()
+
+    Write-Host "Search tenant for MSSQL databases ..."
+
+    # Go to the resource graph and get databases via Kusto query
+    $query = @"
+Resources
+| where type =~ 'microsoft.sql/servers/databases'
+| where tags['alerting'] == 'enabled'
+| where tags['managedby'] == 'test'
+| project id
+"@
+    # return Search-AzGraph -Query $query
+    return @()
+}
+
 function Get-PlainTextSecret {
     param (
         [Parameter(Mandatory = $true)]
@@ -42,6 +60,26 @@ function Get-PlainTextSecret {
     $secret = Get-AzKeyVaultSecret $env:SQL_MONITORING_KEY_VAULT -Name $SecretName
     # Convert from secret string to string
     return [System.Net.NetworkCredential]::new("", $secret.SecretValue).Password
+}
+
+function Get-DatabaseFromConnectionString {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ConnectionString
+    )
+    return "unknown"
+    # TODO: Implement function logic
+}
+
+function Remove-DatabaseFromList {
+    param (
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyCollection()]
+        [array]$List,
+        [Parameter(Mandatory = $true)]
+        [string]$Database
+    )
+    # TODO: Implement function logic
 }
 
 function Test-DatabaseConnection {
@@ -92,6 +130,7 @@ function Invoke-DatabaseMonitoring {
     param()
 
     Connect-AzAccount -Identity
+    $dbs = @($(Get-QbyDatabasesInTenant))
 
     try {
         $con_strings = Get-AzKeyVaultSecret $env:SQL_MONITORING_KEY_VAULT -ErrorAction Stop
@@ -130,6 +169,17 @@ function Invoke-DatabaseMonitoring {
         else {
             Send-MonitoringEvent -Message $error_string -Severity "CRITICAL"
         }
+
+        # Database has been monitored, remove from tenant-wide list
+        $dbname = Get-DatabaseFromConnectionString $con
+        if (![string]::IsNullOrWhiteSpace($dbname)) {
+            Remove-DatabaseFromList -List $dbs -Database $dbname
+        }
+    }
+
+    # Go over remaining list of unmonitored databases
+    foreach ($db in $dbs) {
+        Send-MonitoringEvent -Message "Database is not being monitored!" -Severity "WARNING"
     }
 }
 
