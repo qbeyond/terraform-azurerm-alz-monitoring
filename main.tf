f1134a2947755c7c0afe87d2655c9e61b99a24cd
Cuevas <juan.cuevas@qbeyond.de>
2026-06-03 14:42:56 +0200

    Fixed action group parameters name

--- main.tf
+++ main.tf
@@ -32,11 +32,11 @@ resource "azurerm_monitor_action_group" "optional" {
 resource "azurerm_monitor_action_group" "forwarder" {
   name                = "EventPipelineForwarder_AG_1"
   resource_group_name = var.log_analytics_workspace.resource_group_name
-  short_name          = "agepforwarder"
+  short_name          = "agepfwd"
   tags                = var.tags
 
   webhook_receiver {
-    name                    = "whepforwarder"
+    name                    = "whepfwd"
     service_uri             = replace(var.event_pipeline_config.service_uri_forwarder, "{{secret}}", var.secret_forwarder)
     use_common_alert_schema = true
   }
