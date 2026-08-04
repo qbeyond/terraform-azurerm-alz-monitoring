6765ab1740fe9c95fe303f70da0a6391c633ba97
QBY-MauriceBaerisch <Maurice.Baerisch@qbeyond.de>
2026-08-04 11:13:31 +0000

    chore(reservations): improve naming

--- reservations.tf
+++ reservations.tf
@@ -9,7 +9,7 @@ resource "azurerm_automation_module" "az_reservations" {
   }
 }
 
-resource "azurerm_automation_runbook" "reservation_to_law" {
+resource "azurerm_automation_runbook" "reservations_to_law" {
   count                   = var.reservations.enabled ? 1 : 0
   name                    = "Import-ReservationsToLogAnalytics"
   location                = var.automation_account.location
@@ -44,25 +44,25 @@ resource "azurerm_automation_schedule" "once_monthly" {
   start_time = time_static.monthly_start[0].rfc3339
 }
 
-resource "azurerm_automation_job_schedule" "reservation_to_law" {
+resource "azurerm_automation_job_schedule" "reservations_to_law" {
   count                   = var.reservations.enabled ? 1 : 0
   resource_group_name     = var.automation_account.resource_group_name
   automation_account_name = var.automation_account.name
   schedule_name           = azurerm_automation_schedule.once_monthly[0].name
-  runbook_name            = azurerm_automation_runbook.reservation_to_law[0].name
+  runbook_name            = azurerm_automation_runbook.reservations_to_law[0].name
 
   parameters = {
     customerid = var.log_analytics_workspace.workspace_id
   }
 
   lifecycle {
-    replace_triggered_by = [azurerm_automation_runbook.reservation_to_law[0]]
+    replace_triggered_by = [azurerm_automation_runbook.reservations_to_law[0]]
   }
 }
 
-resource "azurerm_monitor_action_group" "reservationsexpiry" {
+resource "azurerm_monitor_action_group" "reservationexpiry" {
   count               = var.reservations.enabled ? 1 : 0
-  name                = "ag-${local.customer_code}-prd-reservationsexpiry"
+  name                = "ag-${local.customer_code}-prd-reservationexpiry"
   resource_group_name = var.log_analytics_workspace.resource_group_name
   short_name          = "reservations"
   tags                = var.tags
@@ -76,7 +76,7 @@ resource "azurerm_monitor_action_group" "reservationsexpiry" {
   }
 }
 
-resource "azurerm_monitor_scheduled_query_rules_alert_v2" "reservationsexpiry" {
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "reservationexpiry" {
   count               = var.reservations.enabled ? 1 : 0
   name                = "alr-prd-ReservationExpiry-res-law-logsea-warn-01"
   location            = var.log_analytics_workspace.location
@@ -92,11 +92,11 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "reservationsexpiry" {
   window_duration      = "P2D"
 
   action {
-    action_groups = [azurerm_monitor_action_group.reservationsexpiry[0].id]
+    action_groups = [azurerm_monitor_action_group.reservationexpiry[0].id]
   }
 
   criteria {
-    query                   = file("${path.module}/queries/reservations.kusto")
+    query                   = file("${path.module}/queries/reservationexpiry.kusto")
     time_aggregation_method = "Count"
     operator                = "GreaterThan"
     threshold               = 0
