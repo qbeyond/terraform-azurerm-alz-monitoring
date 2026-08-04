55985d6fb41a88dc5718aa41c4e842edf2485a6b
QBY-MauriceBaerisch <Maurice.Baerisch@qbeyond.de>
2026-08-04 07:50:32 +0000

    feat(runbook): make reservation runbook optional
    by using count on var.reservations.enabled boolean

--- reservations.tf
+++ reservations.tf
@@ -1,4 +1,5 @@
 resource "azurerm_automation_module" "az_reservations" {
+  count                   = var.reservations.enabled ? 1 : 0
   name                    = "Az.Reservations"
   resource_group_name     = var.automation_account.resource_group_name
   automation_account_name = var.automation_account.name
@@ -9,6 +10,7 @@ resource "azurerm_automation_module" "az_reservations" {
 }
 
 resource "azurerm_automation_runbook" "reservation_to_law" {
+  count                   = var.reservations.enabled ? 1 : 0
   name                    = "Import-ReservationsToLogAnalytics"
   location                = var.automation_account.location
   resource_group_name     = var.automation_account.resource_group_name
@@ -21,10 +23,12 @@ resource "azurerm_automation_runbook" "reservation_to_law" {
 }
 
 resource "time_static" "monthly_start" {
+  count   = var.reservations.enabled ? 1 : 0
   rfc3339 = timeadd(timestamp(), "24h")
 }
 
 resource "azurerm_automation_schedule" "once_monthly" {
+  count                   = var.reservations.enabled ? 1 : 0
   name                    = "aas-Import-ReservationsToLogAnalytics-Once-Monthly"
   resource_group_name     = var.automation_account.resource_group_name
   automation_account_name = var.automation_account.name
@@ -33,20 +37,21 @@ resource "azurerm_automation_schedule" "once_monthly" {
   interval   = 1
   month_days = [1]
 
-  start_time = time_static.monthly_start.rfc3339
+  start_time = time_static.monthly_start[0].rfc3339
 }
 
 resource "azurerm_automation_job_schedule" "reservation_to_law" {
+  count                   = var.reservations.enabled ? 1 : 0
   resource_group_name     = var.automation_account.resource_group_name
   automation_account_name = var.automation_account.name
-  schedule_name           = azurerm_automation_schedule.once_monthly.name
-  runbook_name            = azurerm_automation_runbook.reservation_to_law.name
+  schedule_name           = azurerm_automation_schedule.once_monthly[0].name
+  runbook_name            = azurerm_automation_runbook.reservation_to_law[0].name
 
   parameters = {
     customerid = var.log_analytics_workspace.workspace_id
   }
 
   lifecycle {
-    replace_triggered_by = [azurerm_automation_runbook.reservation_to_law]
+    replace_triggered_by = [azurerm_automation_runbook.reservation_to_law[0]]
   }
 }
